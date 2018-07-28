fd75900ac1dddcbad612345e81252c23b621baf0
antsuma <anton.nordgren1@gmail.com>
2018-07-28 23:42:42 +0200

    Added collision whe bullets is outside canvas

--- src.js
+++ src.js
@@ -5,6 +5,7 @@ const Game = {
     playerImage : undefined,
     backgroundImage : undefined,
     bulletImage : undefined,
+    nrOfEnemies : 1,
     enemies : []
 }
 
@@ -26,9 +27,35 @@ Game.start = function() {
     document.onmousemove = handleMouseMove;
     document.onmousedown = handleMouseDown;
 
-    Game.enemies.push(new Enemy(100, 100, 0));
-
+    Game.spawnEnemies(Game.nrOfEnemies);
     Game.mainloop();
+}
+
+Game.spawnEnemies = function(nrOfEnemies) {
+    for(let i = 0; i < nrOfEnemies; i++ ) {
+        let dirX = Math.ceil(Math.random() * 2) == 1 ? 1 : -1;
+        let dirY = Math.ceil(Math.random() * 2) == 1 ? 1 : -1;
+        let enemyWidth = 100;
+        let enemyHeight = 76;
+        let posX = Math.ceil(Math.random() * Game.canvas.width);
+        let posY = Math.ceil(Math.random() * Game.canvas.height);
+
+        if(posX < enemyWidth) {
+            posX = enemyWidth + 10;
+        }
+        else if(posX > Game.canvas.width - enemyWidth) {
+            posX = Game.canvas.width - enemyWidth - 10;
+        }
+
+        if(posY < enemyHeight) {
+            posY = enemyHeight + 10;
+        }
+        else if(posY > Game.canvas.height - enemyHeight) {
+            posY = Game.canvas.height - enemyHeight - 10;
+        }
+
+        Game.enemies.push(new Enemy(posX, posY, 0, dirX, dirY));
+    }
 }
 
 Game.clearScreen = function() {
@@ -47,8 +74,11 @@ Game.renderPlayer = function() {
    let opposite = Mouse.x - Player.pos.x - 160;
    let adjecent = Mouse.y - Player.pos.y - 30;
    Game.drawImage(Game.playerImage, Player.pos, -Math.atan2(opposite * -1, adjecent * -1), Player.origin);
+   /*
+   Game.canvasContext.fillStyle = "white";
    Game.canvasContext.fillText("Player.x : " + Player.pos.x, 10 , 30);
    Game.canvasContext.fillText("Player.y : " + Player.pos.y, 10 , 60);
+   */
 }
 
 Game.update = function() {
@@ -89,7 +119,7 @@ document.addEventListener('DOMContentLoaded', Game.start);
 const Player = {
     width : 100,
     height : 80,
-    pos : { x: 400, y : 300 },
+    pos : { x: 800, y : 450 },
     origin : {x : 50, y : 40 },
     velX : 0,
     velY : 0,
@@ -118,7 +148,7 @@ Player.shoot = function(r) {
     Player.bullets.push(newBullet);
 }
 
-function Bullet(_x, _y, r){
+function Bullet(_x, _y, r,){
     this.pos = { x : _x, y : _y},
     this.width = 5;
     this.height = 5;
@@ -136,18 +166,28 @@ Bullet.bulletHandler = function() {
             Player.bullets[i].pos.y += Player.bullets[i].velY;
             Player.bullets[i].pos.x += Player.bullets[i].velX;
         }
+
+        for(let i = 0; i < Player.bullets.length; i++) {
+            if(Player.bullets[i].pos.x < 0 || Player.bullets[i].pos.x > Game.canvas.width ||
+               Player.bullets[i].pos.y < 0 || Player.bullets[i].pos.y > Game.canvas.height) {
+                   
+                Player.bullets.splice(Player.bullets[i], 1);
+            }
+        }
     }
 }
 
-function Enemy(_x, _y, r ) {
+function Enemy(_x, _y, r, dirX, dirY) {
     this.pos = { x : _x, y : _y },
     this.width = 100;
     this.height = 76;
-    this.speed = 2;
+    this.speed = 0.6;
     this.origin = { x : 2.5, y : 2.5 };
     this.r = r;
-    this.velX = Math.sin(r) * this.speed;
-    this.velY = Math.cos(r) * this.speed;
+    this.dirX = dirX;
+    this.dirY = dirY;
+    this.velX = dirX * this.speed;
+    this.velY = dirY * this.speed;
     this.bullets = [];
 }
 
@@ -155,9 +195,13 @@ Enemy.update = function() {
     if(Game.enemies.length !== 0) {
         for(let i = 0; i < Game.enemies.length; i++) {
             if(Game.enemies[i].pos.x > Game.canvas.width - Game.enemies[i].width || Game.enemies[i].pos.x < 0) {
-                Game.enemies[i].speed = Game.enemies[i].speed * -1;
+                Game.enemies[i].velX = Game.enemies[i].velX * -1;
             }
-            Game.enemies[i].pos.x += Game.enemies[i].speed;
+            if(Game.enemies[i].pos.y > Game.canvas.height - Game.enemies[i].height || Game.enemies[i].pos.y < 0) {
+                Game.enemies[i].velY = Game.enemies[i].velY * -1;
+            }
+            Game.enemies[i].pos.x += Game.enemies[i].velX * Game.enemies[i].speed;
+            Game.enemies[i].pos.y += Game.enemies[i].velY * Game.enemies[i].speed;
         }
     }
 }
